e272f3570dfc2edac87e725a913d75784f67cc70
Varun Chaudhary <varunc105@gmail.com>
2025-04-20 18:16:01 +0530

    fix:  hash router instead of browser router

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import "./App.css";
 import Certificates from './Components/Certificates/Certificates';
